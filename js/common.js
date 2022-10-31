bbb2f0ce2e2a43d0e4b80e8f022261602d995ab3
shkim <shkim@iea.co.kr>
2022-10-31 09:53:00 +0900

    첫커밋

---
+++ js/common.js
@@ -0,0 +1,24 @@
+$(function(){
+    /* 팝업 */
+    $(".pop-btn").on("click", function(){
+        $("body").css("overflow","hidden");
+        if($(this).hasClass("pop1")){
+            $(".popup.pop1").addClass("on");
+        }else if($(this).hasClass("pop2")){
+            $(".popup.pop2").addClass("on");
+        }else if($(this).hasClass("pop3")){
+            $(".popup.pop3").addClass("on");
+        }
+    });
+    $(".popup").on("click", function(e){
+        if(e.target === this){
+            $("body").css("overflow","auto");
+            $(this).removeClass("on");
+        }
+    });
+    $(".popup .pop-close, .popup .ok-btn").on("click", function(){
+        $("body").css("overflow","auto");
+        $(this).parents(".popup").removeClass("on");
+    });
+    /* // 팝업 */
+});
