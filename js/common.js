12f180f80a54b5b1b20490d42ebd4592464e9407
shkim <shkim@iea.co.kr>
2022-11-16 14:50:08 +0900

    사이드바 js prepend로 변경

--- js/common.js
+++ js/common.js
@@ -1,75 +1,77 @@
 $(function(){
 
     /* 사이드바 연결 */
-    $("body").prepend(
-      "<div class='side-menu'>\n" +
-      "    <ul class='main-menu'>\n" +
-      "        <li><a href='#'><img src='../img/home-icon.svg' alt=''><img src='../img/home-wt-icon.svg' class='icon-focus'></a>\n" +
-      "        </li>\n" +
-      "        <li><a href=#><img src='../img/group-icon.svg' alt=''><img src='../img/group-wt-icon.svg' class='icon-focus'></a>\n" +
-      "            <ul class='second-menu'>\n" +
-      "                <li><a href='#'>배정조회</a></li>\n" +
-      "                <li><a href='#'>서비스 관리<img src='../img/down-arrow.svg'></a>\n" +
-      "                    <ul class='third-menu'>\n" +
-      "                        <li><a href='#'>서비스 조회</a></li>\n" +
-      "                        <li><a href='#'>월간 서비스 계획</a></li>\n" +
-      "                    </ul>\n" +
-      "                </li>\n" +
-      "            </ul>\n" +
-      "        </li>\n" +
-      "        <li><a href='#'><img src='../img/assign-icon.svg' alt=''><img src='../img/assign-wt-icon.svg' class='icon-focus'></a>\n" +
-      "            <ul class='second-menu'>\n" +
-      "                <li><a href='#'>교육이수현황 조회</a></li>\n" +
-      "            </ul>\n" +
-      "        </li>\n" +
-      "        <li><a href='#'><img src='../img/find-icon.svg' alt=''><img src='../img/find-wt-icon.svg' class='icon-focus'></a>\n" +
-      "            <ul class='second-menu'>\n" +
-      "                <li><a href='#'>급여 조회</a></li>\n" +
-      "            </ul>\n" +
-      "        </li>\n" +
-      "        <li><a href='#'><img src='../img/note-icon.svg' alt=><img src='../img/note-wt-icon.svg' class='icon-focus'></a>\n" +
-      "            <ul class='second-menu'>\n" +
-      "                <li><a href='#'>공지사항</a></li>\n" +
-      "                <li><a href='#'>커뮤니티</a></li>\n" +
-      "                <li><a href='#'>1:1문의</a></li>\n" +
-      "            </ul>\n" +
-      "        </li>\n" +
-      "        <li><a href='#'><img src='../img/setting-icon.svg' alt=''><img src='../img/setting-wt-icon.svg' class='icon-focus'></a>\n" +
-      "            <ul class='second-menu'>\n" +
-      "                <li><a href='#'>서비스현황<img src='../img/down-arrow.svg'></a>\n" +
-      "                    <ul class='third-menu'>\n" +
-      "                        <li><a href='#'>돌보미 정보</a></li>\n" +
-      "                        <li><a href='#'>이용자 정보</a></li>\n" +
-      "                        <li><a href='#'>서비스 모니터링</a></li>\n" +
-      "                    </ul>\n" +
-      "                </li>\n" +
-      "                <li><a href='#'>사업실적<img src='../img/down-arrow.svg'></a>\n" +
-      "                    <ul class='third-menu'>\n" +
-      "                        <li><a href='#'>지역 현황</a></li>\n" +
-      "                        <li><a href='#'>돌봄 서비스 현황</a></li>\n" +
-      "                        <li><a href='#'>장애아 돌보미 현황</a></li>\n" +
-      "                        <li><a href='#'>휴식지원 프로그램 현황</a></li>\n" +
-      "                        <li><a href='#'>연간 사업 이용만족도 통계</a></li>\n" +
-      "                        <li><a href='#'>돌보미 교육만족도 관리</a></li>\n" +
-      "                        <li><a href='#'>교육이수확인</a></li>\n" +
-      "                    </ul>\n" +
-      "                </li>\n" +
-      "                <li><a href='#'>시스템총괄<img src='../img/down-arrow.svg'></a>\n" +
-      "                    <ul class='third-menu'>\n" +
-      "                        <li><a href='#'>돌봄서비스 소개 관리</a></li>\n" +
-      "                        <li><a href='#'>휴식지원프로그램 소개 관리</a></li>\n" +
-      "                        <li><a href='#'>오시는 길 관리</a></li>\n" +
-      "                        <li><a href='#'>관련 사이트 관리</a></li>\n" +
-      "                        <li><a href='#'>시스템 파라미터 관리</a></li>\n" +
-      "                    </ul>\n" +
-      "                </li>\n" +
-      "                <li><a href='#'>메시지발송</a></li>\n" +
-      "            </ul>\n" +
-      "        </li>\n" +
-      "    </ul>\n" +
-      "    <div class=sub-menu-bg></div>\n" +
-      "</div>"
-    );
+    if(!$("body").hasClass("no-commons")){
+        $("body").prepend(
+          "<div class='side-menu'>\n" +
+          "    <ul class='main-menu'>\n" +
+          "        <li><a href='#'><img src='../img/home-icon.svg' alt=''><img src='../img/home-wt-icon.svg' class='icon-focus'></a>\n" +
+          "        </li>\n" +
+          "        <li><a href=#><img src='../img/group-icon.svg' alt=''><img src='../img/group-wt-icon.svg' class='icon-focus'></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>배정조회</a></li>\n" +
+          "                <li><a href='#'>서비스 관리<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>서비스 조회</a></li>\n" +
+          "                        <li><a href='#'>월간 서비스 계획</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/assign-icon.svg' alt=''><img src='../img/assign-wt-icon.svg' class='icon-focus'></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>교육이수현황 조회</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/find-icon.svg' alt=''><img src='../img/find-wt-icon.svg' class='icon-focus'></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>급여 조회</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/note-icon.svg' alt=><img src='../img/note-wt-icon.svg' class='icon-focus'></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>공지사항</a></li>\n" +
+          "                <li><a href='#'>커뮤니티</a></li>\n" +
+          "                <li><a href='#'>1:1문의</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "        <li><a href='#'><img src='../img/setting-icon.svg' alt=''><img src='../img/setting-wt-icon.svg' class='icon-focus'></a>\n" +
+          "            <ul class='second-menu'>\n" +
+          "                <li><a href='#'>서비스현황<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>돌보미 정보</a></li>\n" +
+          "                        <li><a href='#'>이용자 정보</a></li>\n" +
+          "                        <li><a href='#'>서비스 모니터링</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>사업실적<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>지역 현황</a></li>\n" +
+          "                        <li><a href='#'>돌봄 서비스 현황</a></li>\n" +
+          "                        <li><a href='#'>장애아 돌보미 현황</a></li>\n" +
+          "                        <li><a href='#'>휴식지원 프로그램 현황</a></li>\n" +
+          "                        <li><a href='#'>연간 사업 이용만족도 통계</a></li>\n" +
+          "                        <li><a href='#'>돌보미 교육만족도 관리</a></li>\n" +
+          "                        <li><a href='#'>교육이수확인</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>시스템총괄<img src='../img/down-arrow.svg'></a>\n" +
+          "                    <ul class='third-menu'>\n" +
+          "                        <li><a href='#'>돌봄서비스 소개 관리</a></li>\n" +
+          "                        <li><a href='#'>휴식지원프로그램 소개 관리</a></li>\n" +
+          "                        <li><a href='#'>오시는 길 관리</a></li>\n" +
+          "                        <li><a href='#'>관련 사이트 관리</a></li>\n" +
+          "                        <li><a href='#'>시스템 파라미터 관리</a></li>\n" +
+          "                    </ul>\n" +
+          "                </li>\n" +
+          "                <li><a href='#'>메시지발송</a></li>\n" +
+          "            </ul>\n" +
+          "        </li>\n" +
+          "    </ul>\n" +
+          "    <div class=sub-menu-bg></div>\n" +
+          "</div>"
+        );
+    }
 
     /* 팝업 */
     $(".pop-btn").on("click", function(){
