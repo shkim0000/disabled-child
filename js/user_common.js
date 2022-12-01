27640bfe69514f5cd9c8f37f1ab977497400dd38
shkim <shkim@iea.co.kr>
2022-12-01 09:29:31 +0900

    사용자 페이지 가이드 작성

--- js/user_common.js
+++ js/user_common.js
@@ -7,6 +7,7 @@ $(function(){
         });
     });
 
+    /* 팝업 열기 */
     $(".pop-btn").on("click", function() {
         $("body").addClass("on");
         if ($(this).hasClass("pop1")) {
@@ -14,6 +15,7 @@ $(function(){
         }
     });
 
+    /* 팝업 닫기 (배경 선택) */
     $(".popup").on("click", function(e){
         if(e.target === this){
             $("body").removeClass("on");
@@ -21,8 +23,16 @@ $(function(){
         }
     });
 
+    /* 팝업 닫기 버튼 */
     $(".popup .pop-close, .popup .close-btn").on("click", function(){
         $("body").css("overflow","auto");
         $(this).parents(".popup").removeClass("on");
+    });
+
+    /* 탭 버튼 */
+    $(".tab-wrap .btn").on("click", function(){
+        let tabNum = $(this).parent().index();
+        $(".tab-wrap .btn").removeClass("on").parent().eq(tabNum).find(".btn").addClass("on");
+        $(".tab-box .tabs").removeClass("on").eq(tabNum).addClass("on");
     });
 });
